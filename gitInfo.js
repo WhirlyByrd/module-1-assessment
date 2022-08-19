a362145333f0f5b3b43537554015e7fea2d8c0d5
WhirlyByrd <kara.r.gardner@gmail.com>
2022-08-19 12:03:08 -0500

    finished gitInfo.js

--- gitInfo.js
+++ gitInfo.js
@@ -10,6 +10,7 @@
 */
 
 //CODE HERE
+let gitDefinition = 'Git is free and open source software for distributed version control: tracking changes in any set of files, usually used for coordinating work among programmers collaboratively developing source code during software development.'
 
 //////////////////PROBLEM 2////////////////////
 /*
@@ -18,7 +19,7 @@
 */
 
 //CODE HERE
-
+let gitHubDefinition = 'GitHub, Inc., is an Internet hosting service for software development and version control using Git.'
 //////////////////PROBLEM 3////////////////////
 /*
     Create a variable called 'gitInitDefinition'.  
@@ -26,7 +27,7 @@
 */
 
 //CODE HERE
-
+let gitInitDefinition = 'initializes a new git repo'
 //////////////////PROBLEM 4////////////////////
 /*
     Create a variable called 'gitCloneDefinition'.  
@@ -34,7 +35,7 @@
 */
 
 //CODE HERE
-
+let gitCloneDefinition = 'git clone is used to make a clone or copy of a repo into a new directory, at another location'
 //////////////////PROBLEM 5////////////////////
 /*
     Create a variable called 'gitStatusDefinition'.  
@@ -42,6 +43,7 @@
 */
 
 //CODE HERE
+let gitStatusDefinition = 'git status is used to display paths that have differences between the index file and the current HEAD commit, paths that have differences between the working tree and the index file, and paths in the working tree that are not tracked by Git. '
 
 //////////////////PROBLEM 6////////////////////
 /*
@@ -53,6 +55,7 @@
 */
 
 //CODE HERE
+let gitAddDefinition = 'git add . adds new or changed files in your working directory to the Git staging area.'
 
 //////////////////PROBLEM 7////////////////////
 /*
@@ -65,7 +68,8 @@
 
 //CODE HERE
 
-
+let gitCommitDefinition = 'The "commit" command is used to save your changes to the local repository.'
+let gitCommitCode = 'The first commit that a developer makes when they start a new software project is called the initial commit.'
 //////////////////PROBLEM 8////////////////////
 /*
     Create a variable called 'gitPushDefinition'.  
@@ -73,3 +77,5 @@
 */
 
 //CODE HERE
+
+let gitPushDefinition = 'git push saves the code into a github repository'
