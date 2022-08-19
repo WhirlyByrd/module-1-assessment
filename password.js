31764d111119ac3cc270834f6b270496d38bb294
WhirlyByrd <kara.r.gardner@gmail.com>
2022-08-19 13:21:16 -0500

    added ability to check for special chars

--- password.js
+++ password.js
@@ -1,29 +1,31 @@
 // create a variable called password, set value as a string
 
-const password = 'Checkpass1'
+const password = 'Checkpass1#'
 
 // write conditional statement that checks if password meets the following constraints:
-
-
 // At least 10 chars long
 // contains at least 1 letter and 1 number
-
 // If the password meets the constraint, console log a success message.
-
 // If the password fails the constraint, console log a failure message.
+
+
+
+
 
 
 
 // /\d/ = checks for any digit from 0 - 9
 // .test() = returns 'true' if expression is matched in string else returns 'false'
-
 // extra credit : I'm checking for capital letters as well using /[A-Z]/
+// extra credit : I'm checking for special characters with /[-+_!@#$%^&*., ?]/
 
-if (password.length >= 10 && /\d/.test(password) && /[A-Z]/) {
+if (password.length >= 10 && /\d/.test(password) && /[A-Z]/.test(password) && /[-+_!@#$%^&*., ?]/.test(password) ) {
     console.log('Success!')
 } else {
     console.log('Password failure!')
 }
+
+
 
 // Test out your conditional by changing the value of your password variable 
 // and running your file!
