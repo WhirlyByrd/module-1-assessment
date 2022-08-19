105cba24dac066fe7c4b1cd53d6045cf1c4e898f
WhirlyByrd <kara.r.gardner@gmail.com>
2022-08-19 11:48:59 -0500

    arrays.js answered

--- arrays.js
+++ arrays.js
@@ -4,12 +4,15 @@
 const faveColors = ['red', 'green', 'black']
 
 //CODE HERE
+let colorCopy = faveColors.slice(0,3)
+console.log(colorCopy)
 
 //////////////////PROBLEM 2////////////////////
 /* Add a fourth color to the end of the 'colorCopy' array using the push method. */
 
 //CODE HERE
-
+colorCopy.push('yellow')
+console.log(colorCopy)
 //////////////////PROBLEM 3////////////////////
 /*
   Using an array method, create a new array called 'middleNums' from the 
@@ -19,7 +22,8 @@ const faveColors = ['red', 'green', 'black']
 const numbers = [1, 2, 3, 4, 5]
 
 //CODE HERE
-
+let middleNums = numbers.splice(1,3) //.splice(index, how many)
+console.log(middleNums)
 
 //////////////////PROBLEM 4////////////////////
 /* 
@@ -27,6 +31,7 @@ const numbers = [1, 2, 3, 4, 5]
   Using a for loop, loop over the array called 'bigOrSmallArray', and check to see if the 
   number in the array is GREATER than 100. 
   If it is, push 'big' as a string to the answers array. 
+  
   If the number is LESS than or EQUAL to 100, push 'small' as a string 
   to the answers array. 
 */
@@ -34,3 +39,14 @@ const numbers = [1, 2, 3, 4, 5]
 const bigOrSmallArray = [1, 99, 42, 69, 102, -10, 159, 352]
 
 // CODE HERE
+let answers = []
+
+for(i = 0; i <= bigOrSmallArray.length; i++){
+  if (bigOrSmallArray[i] > 100){
+    answers.push(bigOrSmallArray[i])
+  } else (bigOrSmallArray[i] <= 100);{
+    console.log('small')
+   
+  }
+}
+console.log(answers)
